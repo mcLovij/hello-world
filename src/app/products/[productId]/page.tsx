d4ab4e599642b2d15d37471da65c103b805ab4f1
KoKoMaDaKaSka <seyhasun036@gmail.com>
2024-05-01 16:00:37 +0800

    Initial commit:Dynamic Routing

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,5 @@
-export default function ProductDetail({params}){
+export default function ProductDetail({params,}: {
+    params: { productId: String }
+}) {
     return <h1>Product Detail of {params.productId}</h1>
 }
