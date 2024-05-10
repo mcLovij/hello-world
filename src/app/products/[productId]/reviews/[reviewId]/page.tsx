409f451a92d053ce6971e30780ccc39e5c976ebe
KoKoMaDaKaSka <seyhasun036@gmail.com>
2024-05-10 09:58:05 +0800

    Initial commit:DNested Dynamic Routing

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,17 @@
-export default function ReviewDetail({params}: {
-    params: {
-        productId: String
-        reviewId: String
+export  default function ProductReview({params}:{
+    params:{
+        productId:String
+        reviewId:String
     }
-}) {
-    return <h1>Review {params.productId} of Product {params.reviewId}</h1>
+}){
+    return <h1>product {params.productId} and review {params.reviewId}</h1>
 }
+
+// export default function ReviewDetail({params}: {
+//     params: {
+//         productId: String
+//         reviewId: String
+//     }
+// }) {
+//     return <h1>Review {params.productId} of Product {params.reviewId}</h1>
+// }
